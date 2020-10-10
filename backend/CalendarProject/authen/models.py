e932a2479e2f546456ac8a8bb53dc11c28e63cbf
adouniH <Houssemadouni11@gmail.com>
2020-10-10 04:06:34 +0200

    post delete user

--- backend/CalendarProject/authen/models.py
+++ backend/CalendarProject/authen/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from django.conf import settings
 from django.db.models.signals import post_save
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from django.db import models
@@ -12,7 +13,7 @@ class Account(models.Model):
     def __str__(self):
         return self.code
 
-    code = models.CharField(max_length=30)
+    code = models.CharField(max_length=30, unique=True)
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
@@ -24,3 +25,9 @@ class Account(models.Model):
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+@receiver(post_delete, sender=Account)
+def post_delete_user(sender, instance, *args, **kwargs):
+    if instance.user:
+        instance.user.delete()
